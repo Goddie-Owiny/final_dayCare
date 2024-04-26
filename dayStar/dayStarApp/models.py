ee4393b02d39a67304832319c83e1b781a57faa1
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2024-04-27 02:32:46 +0300

    Now adding saved sitters and baby to the index page

--- dayStar/dayStarApp/models.py
+++ dayStar/dayStarApp/models.py
@@ -7,13 +7,21 @@ class Sitter(models.Model):
     name = models.CharField(max_length=100, blank=False)
     age = models.IntegerField(default=0, blank=False)
     gender = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    contact = models.CharField(max_length=13)
+    education = models.CharField(max_length=255, blank=False, null=True)
+    religion = models.CharField(max_length=100, blank=True)
+    next_of_kin = models.CharField(max_length=200, blank=False, default= None)
+    sitter_number = models.CharField(max_length=10, unique=True, blank=False, default=None)
+    NIN = models.CharField(max_length=14, blank=False, null=True)
 
+    
     # babys Model
 class Baby(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    age = models.IntegerField(default=0)
     gender = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    babyNum = models.IntegerField()
-    babyBringer = models.CharField(max_length=100)
-    parentName = models.CharField(max_length=100)
+    baby_Number = models.IntegerField()
+    baby_Bringer = models.CharField(max_length=100)
+    parent_Name = models.CharField(max_length=100)
--- dayStar/dayStarApp/views.py
+++ dayStar/dayStarApp/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import *
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return render(request, 'dayStarApp/index.html')
+    all_sitters = Sitter.objects.all()
+    all_babys = Baby.objects.all()
+    count_sitters = Sitter.objects.count()
+    count_babys = Baby.objects.count()
+    context = {
+        'count_sitters': count_sitters,
+        'count_babys': count_babys,
+        'all_sitters': all_sitters,
+        'all_babys': all_babys,
+    }
+    template = loader.get_template('dayStarApp/index.html')
+    return HttpResponse(template.render(context))
+   
 
 def sitter(request):
     addSitterForm = Sitter_regForm()
-    allSitters = Sitter.objects.all()
-    context = {
-        'allSitters': allSitters,
-    }
+
     return render(request, 'dayStarApp/sitter.html', {'addSitterForm': addSitterForm})
 
 
 def baby(request):
     addBabyForm = Baby_regForm()
-    allBabys = Sitter.objects.all()
-    context = {
-        'allBabys': allBabys,
-    }
-    return render(request, 'dayStarApp/sitter.html', {'addSitterForm': addBabyForm}) 
    
+    return render(request, 'dayStarApp/sitter.html', {'addSitterForm': addBabyForm}) 
+    
